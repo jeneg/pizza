4aa5af3c8878a11c8d8ba46efea07b2656256963
jeneg <polizey565@gmail.com>
2017-08-11 15:30:35 +0300

    [ng2] updated ng

--- ng2/src/app/shared/shared.module.ts
+++ ng2/src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {SectionTitleComponent} from './section-title/section-title.component';
 import {HttpModule} from '@angular/http';
-import { ButtonComponent } from './button/button.component';
 import { CartTotalComponent } from './cart-total/cart-total.component';
 import { FooterComponent } from './footer/footer.component';
 import {RouterModule} from "@angular/router";
@@ -18,7 +17,6 @@ import { HeaderEffectsDirective } from './header/header-effects.directive';
   exports: [
     HeaderComponent,
     SectionTitleComponent,
-    ButtonComponent,
     CartTotalComponent,
     FooterComponent,
     HeaderEffectsDirective
@@ -26,7 +24,6 @@ import { HeaderEffectsDirective } from './header/header-effects.directive';
   declarations: [
     HeaderComponent,
     SectionTitleComponent,
-    ButtonComponent,
     CartTotalComponent,
     FooterComponent,
     HeaderEffectsDirective
